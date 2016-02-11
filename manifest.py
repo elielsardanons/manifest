5368194d803112422b82103e4ad45c4f41ce6725
eliel <eliel@elielsardanons.(none)>
2016-02-11 09:05:06 -0300

    Listen on 0.0.0.0:80

--- manifest.py
+++ manifest.py
@@ -48,4 +48,4 @@ def live():
 	return render_template("showLive.html", showFlights = showFlights, skydiverImage = skydiverImage, nextCall = nextCall) 
 
 if __name__ == "__main__":
-	app.run()
+	app.run(host = '0.0.0.0', port = 80)
